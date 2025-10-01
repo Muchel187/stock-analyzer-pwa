ed3b331e61e0a4e340793bd7270aabdc9090a447
JBK <jbk@example.com>
2025-10-02 00:21:51 +0200

    Setup PostgreSQL migrations for Render deployment

--- migrations/add_acknowledged_to_alerts.sql
+++
@@ -1,7 +0,0 @@
--- Add acknowledged column to alerts table
--- For Phase 3 Part 3: Notification Center
-
-ALTER TABLE alerts ADD COLUMN IF NOT EXISTS acknowledged BOOLEAN DEFAULT FALSE;
-
--- Update existing triggered alerts to be acknowledged (so they don't all show up)
-UPDATE alerts SET acknowledged = TRUE WHERE is_triggered = TRUE AND acknowledged IS NULL;
